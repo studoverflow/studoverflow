bb653c76f0d0d6e2f2354372cc435ed55844b8d8
Stephen Beck <mail@stephenbeck.de>
2016-05-13 23:12:08 +0200

    E-Mail smpt passwort reset activated

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'studoverflow@stephenbeck.de', 'name' => 'StudOverflow'],
 
     /*
     |--------------------------------------------------------------------------
