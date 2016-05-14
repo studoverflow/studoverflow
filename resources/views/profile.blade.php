070cb1524b72efb417bf611c9e4b781c843d3c5f
Stephen Beck <mail@stephenbeck.de>
2016-05-14 20:19:08 +0200

    one static profile from database

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+<?php
+
+    $user = App\User::find("1");
+    $profile = App\Profile::find($user->id);
+    
+
+?>
 
 <section class="container-fluid" id="profile">
-    <h1 class="text-center">Profil</h1>
+    <h1 class="text-center">Profil @if (!Auth::guest()) <a href="#" class="editsize">Edit</a> @endif</h1> 
     <section class="container">
         <div class="row">
             <div class="col-md-3 col-md-offset-1">
@@ -19,31 +26,31 @@
             <div class="col-md-4">
                 <p>
                     <b class="profil">User </b>
-                    </br>Enes
+                    </br><?php echo $user->name; ?>
                 </p>
                 <p>
                     <b class="profil">Vorname</b>
-                    </br>{{$profile = "yo"; 
+                    </br> <?php echo $profile->forename; ?>
                 </p>
                 <p>
                     <b class="profil">Nachname</b>
-                    </br>Mustermann
+                    </br><?php echo $profile->surname; ?>
                 </p>
                 <p>
                     <b class="profil">E-Mail</b>
-                    </br>enes@gmx.de
+                    </br><?php echo $user->email; ?>
                 </p>
             </div>
             <div class="col-md-4">
                 <p>
                     <b class="profil">Homepage</b>
-                    </br>www.enes.de
+                    </br><?php echo $profile->page; ?>
                 </p>
                 <p>
-                    <b class="profil">Hochschule</b></br>HTWG-Konstanz (www.htwg-konstanz.de)
+                    <b class="profil">Hochschule</b></br><?php echo $profile->college; ?>
                 </p>
                 <p>
-                    <b class="profil">Studiengang</b></br>Wirtschaftsinformatik
+                    <b class="profil">Studiengang</b></br><?php echo $profile->course; ?>
                 </p>
             </div>
         </div>
