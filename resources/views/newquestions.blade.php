058a4cdfb0ba3a336538f53dc554b7e6ff86a1d5
Stephen Beck <mail@stephenbeck.de>
2016-05-15 18:27:12 +0200

    new questions sort by date desk and new questions are only 15 day old questions

--- resources/views/newquestions.blade.php
+++ resources/views/newquestions.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<?php $questions = App\Question::all(); ?>
+<!--  <?php $questions = App\Question::all() ?> -->
+<?php $questions = DB::select('select * from questions order by date desc'); ?>
 <section class="container-fluid" id="new">
     <section class="container">
         <article class="row">
@@ -9,8 +10,14 @@
                 <div class="col-sm-12 questionheader">
                     <h1 class="text-center questionheaderfont">Neuste Fragen</h1>
                 </div>
-                
                     @foreach($questions as $question)
+                        <?php
+                            $datecurr = date("Y-m-d");
+                            $datetime1 = new DateTime($datecurr);
+                            $datetime2 = new DateTime($question->date);
+                            $interval = $datetime1->diff($datetime2);
+                        ?>
+                        @if($interval->format('%a') <= "15")
                         <?php $user = App\User::find($question->user_id); ?>
                         <div class="col-sm-12 questionblue">
                             <div class="col-sm-6">
@@ -26,6 +33,7 @@
                                 <i class="fa fa-clock-o" aria-hidden="true"></i> {{$question->date}}
                             </div>
                         </div>
+                        @endif
                     @endforeach
                     
 <!--                 <div class="col-sm-12 questionwhite">
