c76be97488cac6b8d88a9614926e778663d98b1d
Stephen Beck <mail@stephenbeck.de>
2016-05-15 21:21:14 +0200

    question update

--- resources/views/unansweredquestions.blade.php
+++ resources/views/unansweredquestions.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<?php 
-$questions = DB::select('select * from questions order by date desc'); 
-$answers = DB::select('select * from countanswer');
-?>
+<?php $unanswered = DB::select('select * from unanswered'); ?>
 <section class="container-fluid" id="new">
     <section class="container">
         <article class="row">
@@ -12,26 +9,23 @@ $answers = DB::select('select * from countanswer');
                 <div class="col-sm-12 questionheader">
                     <h1 class="text-center questionheaderfont">Beliebte Fragen</h1>
                 </div>
-                @foreach($answers as $answer)
-                    @foreach($questions as $question)
-                        @if($answer->anzahl > 0 && $answer->id==$question->id)
-                            <?php $user = App\User::find($question->user_id); ?>
-                            <div class="col-sm-12 questionblue">
-                                <div class="col-sm-6">
-                                    <b><a href="/question={{$question->id}}">
-                                    <i class="fa fa-question-circle-o" aria-hidden="true"></i> 
-                                    {{$question->titel}}
-                                    </a></b>
-                                </div>
-                                <div class="col-sm-3">
-                                    <b><a href="/profile={{$user->id}}"><i class="fa fa-user" aria-hidden="true"></i> {{$user->name}}</a></b>
-                                </div>
-                                <div class="col-sm-3">
-                                    <i class="fa fa-clock-o" aria-hidden="true"></i> {{$question->date}}
-                                </div>
-                            </div>
-                        @endif
-                    @endforeach
+                @foreach($unanswered as $key)
+                    <?php $question = App\Question::find($key->id); ?>
+                    <?php $user = App\User::find($question->user_id); ?>
+                    <div class="col-sm-12 questionblue">
+                        <div class="col-sm-6">
+                            <b><a href="/question={{$question->id}}">
+                            <i class="fa fa-question-circle-o" aria-hidden="true"></i> 
+                            {{$question->titel}}
+                            </a></b>
+                        </div>
+                        <div class="col-sm-3">
+                            <b><a href="/profile={{$user->id}}"><i class="fa fa-user" aria-hidden="true"></i>{{$user->name}}</a></b>
+                        </div>
+                        <div class="col-sm-3">
+                            <i class="fa fa-clock-o" aria-hidden="true"></i> {{$question->date}}
+                        </div>
+                    </div>
                 @endforeach
             </article>
         </article>
