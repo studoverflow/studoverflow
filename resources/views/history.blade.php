cb98141b6426d1e3398720fdf79f449d485c44e5
Stephen Beck <mail@stephenbeck.de>
2016-05-16 01:17:00 +0200

    kleine anpassungen und verlauf fertig

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<?php $questions = DB::select('select * from questions order by date desc'); ?>
+<?php $questions = DB::select('select * from questions where user_id = ? order by date desc', [Auth::user()->id]); ?>
+<?php $answers = DB::select('select * from answers where user_id = ? order by date desc', [Auth::user()->id]); ?>
 <section class="container-fluid" id="history">
     <section class="container">
         <article class="row">
             <article class="col-sm-12">
                 <div class="col-sm-12 questionheader">
                     <h1 class="text-center questionheaderfont">Verlauf</h1>
+                </div>
+                <div class="col-sm-12 questionwhite">
+                    <h3 class="text-center studoverflow">Fragen</h3>
                 </div>
                 @foreach($questions as $question)
                     <?php $user = App\User::find($question->user_id); ?>
@@ -16,6 +20,25 @@
                             <b><a href="/question={{$question->id}}">
                             <i class="fa fa-question-circle-o" aria-hidden="true"></i> 
                             {{$question->titel}}
+                            </a></b>
+                        </div>
+                        <div class="col-sm-3">
+                            <b><a href="/profile={{$user->id}}"><i class="fa fa-user" aria-hidden="true"></i> {{$user->name}}</a></b>
+                        </div>
+                        <div class="col-sm-3">
+                            <i class="fa fa-clock-o" aria-hidden="true"></i> {{$question->date}}
+                        </div>
+                    </div>
+                @endforeach
+                <div class="col-sm-12 questionwhite">
+                    <h3 class="text-center studoverflow">Antworten</h3>
+                </div>
+                @foreach($answers as $answer) 
+                    <div class="col-sm-12 questionblue">
+                        <div class="col-sm-6">
+                            <b><a href="/question={{$answer->question_id}}">
+                            <i class="fa fa-question-circle-o" aria-hidden="true"></i> 
+                            {{$answer->titel}}
                             </a></b>
                         </div>
                         <div class="col-sm-3">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,70 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-@if(Auth::guest())
 <section class="container-fluid" id="welcome">
     <section class="container">
         <article class="row">
-            <article class="col-sm-12">
-                <div class="col-sm-12 ">
-                    <h1 class="text-center "><h1 class="text-center studoverflow">
+            <article class="col-xs-12">
+                <div class="col-xm-12 ">
+                    <h1 class="text-center"><h1 class="text-center studoverflow">
                     <i class="fa fa-stack-overflow" aria-hidden="true"></i>  StudOverflow</h1></h1>
-                                        <p class="text-center">
-                        <a type="button" href="/register" class="btn btn-black btn-lg btn-huge">
-
-                        jetzt Mitglied werden</a>
+                @if(Auth::guest())
+                    <p class="text-center">
+                        <a type="button" href="/register" class="btn btn-black btn-lg btn-huge">jetzt Mitglied werden</a>
                     </p>
                 </div>  
-            </article>
-        </article>
-        <!--/row-->
-    </section>
-    <!--/container-->
-</section>
-@else
+                @else
 <?php $countquestions = DB::select('select count(*) as count from questions where user_id = ?', [Auth::user()->id]); ?>
 <?php $countanswers = DB::select('select count(*) as count from answers where user_id = ?', [Auth::user()->id]); ?>
-<section class="container-fluid" id="welcome">
-    <section class="container">
-        <article class="row">
-            <article class="col-sm-12">
-                <div class="col-sm-12 marginbottom20">
-                    <h1 class="text-center "><h1 class="text-center studoverflow">
-                    <i class="fa fa-stack-overflow" aria-hidden="true"></i>  StudOverflow</h1></h1>
-                </div>
-                <div col-md-12><h2 class="studoverflow text-center">Willommen zurück {{ Auth::user()->name }}<h2></div>
-                <div class="col-md-12 statsbox text-center">
-                    <h1 class="studoverflow">Deine Statistik</h1>
-                    <div class="col-md-6">User</div>
-                    <div class="col-md-6">{{ Auth::user()->name }}</div>
-                    <div class="col-md-6">Email</div>
-                    <div class="col-md-6">{{ Auth::user()->email }}</div>
+                    <div class="col-md-12"><h2 class="studoverflow text-center">Willommen zurück {{ Auth::user()->name }}<h2></div>
+                    <div class="col-md-12 statsbox text-center">
+                        <h1 class="studoverflow">Deine Statistik</h1>
+                        <div class="col-md-6">User</div>
+                        <div class="col-md-6">{{ Auth::user()->name }}</div>
 
-                    <div class="col-md-6">gestellte Fragen</div>
-                    @foreach($countquestions as $question)
-                        @if($question->count != null)
-                            <div class="col-md-6">{{ $question->count }}</div>
-                        @else
-                            <div class="col-md-6">0</div>
-                        @endif
-                    @endforeach
+                        <div class="col-md-6">gestellte Fragen</div>
+                        @foreach($countquestions as $question)
+                            @if($question->count != null)
+                                <div class="col-md-6">{{ $question->count }}</div>
+                            @else
+                                <div class="col-md-6">0</div>
+                            @endif
+                        @endforeach
 
-                    <div class="col-md-6">gegebene Antworten</div>
-                    @foreach($countanswers as $answer)
-                        @if($answer->count != null)
-                            <div class="col-md-6">{{ $answer->count }}</div>
-                        @else
-                            <div class="col-md-6">0</div>
-                        @endif
-                    @endforeach
-
-                </div>
+                        <div class="col-md-6">gegebene Antworten</div>
+                        @foreach($countanswers as $answer)
+                            @if($answer->count != null)
+                                <div class="col-md-6">{{ $answer->count }}</div>
+                            @else
+                                <div class="col-md-6">0</div>
+                            @endif
+                        @endforeach
+                    </div>
+                @endif
             </article>
-        </article>
         <!--/row-->
     </section>
     <!--/container-->
 </section>
-                @endif  
-
 @endsection
