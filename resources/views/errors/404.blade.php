98aa49d18fb323412111e7e2a2113ff3441f93ab
Stephen Beck <mail@stephenbeck.de>
2016-05-26 20:22:55 +0200

    change

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,9 +2,8 @@
 
 @section('content')
 <section id="error">
-	
-	<h1 class="text-center margintop20">404 File Not Found</h1>
 	<img src="/img/404.png" class="text-center">
+	<h1 class="text-center margintop20">404 File Not Found</h1>
 
 </section>
 @endsection
