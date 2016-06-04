f5ad8de5c7caae68f1a91ecc5541dfd1efbb5625
Markus Jäckle <markus.jaeckle@htwg-konstanz.de>
2016-06-04 15:42:49 +0200

    modified profile and editProfile

--- resources/views/editprofile.blade.php
+++ resources/views/editprofile.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="container-fluid" id="edit">
-    <h1 class="text-center studoverflow margintop80">Profil anpassen</h1>
-    <article class="container marginbottom80">
+<section class="container-fluid margintop40" id="edit">
+    <h1 class="text-center studoverflow">Profil anpassen</h1>
+    <article class="container margintop40">
         <article class="row">
             <div class="col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1">
                 <form enctype="multipart/form-data" action="/editprofile" method="POST">
@@ -15,7 +15,7 @@
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-sm-12 col-md-12">
+                                <div class="col-sm-12 col-md-12 margintop10">
                                     <label>Profilbild ändern</label>
                                     <input type="file" name="avatar">
                                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -24,58 +24,58 @@
                         </div>
                         <div class="col-sm-9 col-md-9">
                             <div class="row" style="border-bottom: 1px solid #dddddd; border-top: 1px solid #dddddd; padding: 10px 0">
-                                <div class="col-md-5">
-                                    <b class="profil">Username</b>
+                                <div class="col-md-3">
+                                    <b class="profil">Username:</b>
                                 </div>
-                                <div class="col-md-7">
+                                <div class="col-md-9">
                                     <input class="form-control input-md" type="text" name="username" value="{{ Auth::user()->name }}" disabled="">
                                 </div>
                             </div>
                             <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
-                                <div class="col-md-5">
-                                    <b class="profil">E-Mail</b>
+                                <div class="col-md-3">
+                                    <b class="profil">E-Mail:</b>
                                 </div>
-                                <div class="col-md-7">
+                                <div class="col-md-9">
                                     <input class="form-control input-md" type="text" name="email" value="{{ Auth::user()->email }}">
                                 </div>
                             </div>
                             <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
-                                <div class="col-md-5">
-                                    <b class="profil">Vorname</b>
+                                <div class="col-md-3">
+                                    <b class="profil">Vorname:</b>
                                 </div>
-                                <div class="col-md-7">
+                                <div class="col-md-9">
                                     <input class="form-control input-md" type="text" name="firstname" value="{{ Auth::user()->firstname }}">
                                 </div>
                             </div>
                             <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
-                                <div class="col-md-5">
-                                    <b class="profil">Nachname</b>
+                                <div class="col-md-3">
+                                    <b class="profil">Nachname:</b>
                                 </div>
-                                <div class="col-md-7">
+                                <div class="col-md-9">
                                     <input class="form-control input-md" type="text" name="lastname" value="{{ Auth::user()->lastname }}">
                                 </div>
                             </div>
                             <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
-                                <div class="col-md-5">
-                                    <b class="profil">Homepage</b>
+                                <div class="col-md-3">
+                                    <b class="profil">Homepage:</b>
                                 </div>
-                                <div class="col-md-7">
+                                <div class="col-md-9">
                                     <input class="form-control input-md" type="text" name="page" value="{{ Auth::user()->page }}">
                                 </div>
                             </div>
                             <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
-                                <div class="col-md-5">
-                                    <b class="profil">Hochschule</b>
+                                <div class="col-md-3">
+                                    <b class="profil">Hochschule:</b>
                                 </div>
-                                <div class="col-md-7">
+                                <div class="col-md-9">
                                     <input class="form-control input-md" type="text" name="college" value="{{ Auth::user()->college }}">
                                 </div>
                             </div>
                             <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
-                                <div class="col-md-5">
-                                    <b class="profil">Studiengang</b>
+                                <div class="col-md-3">
+                                    <b class="profil">Studiengang:</b>
                                 </div>
-                                <div class="col-md-7">
+                                <div class="col-md-9">
                                     <input class="form-control input-md" type="text" name="course" value="{{ Auth::user()->course }}">
                                 </div>
                             </div>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,13 @@
 @section('content')
 <section class="container-fluid margintop40" id="profile">
     <h1 class="text-center studoverflow">Profil 
-    @if (!Auth::guest() && Auth::user()->name == $name) <a href="/editprofile"" class="editsize">Edit</a> @endif</h1>
+        @if (!Auth::guest() && Auth::user()->name == $name)
+            <a href="/editprofile"" class="editsize" style="font-size: 12px">
+                <span class="fa fa-btn fa-pencil"></span>
+                edit
+            </a> 
+        @endif
+    </h1>
     @if (!Auth::guest() && Auth::user()->rights == 'Admin')
         <form class="text-center" action="/profile" method="POST">
             <label>Userrechte:</label>
@@ -18,91 +24,103 @@
             <input type="submit" name="aendern" value="Ändern" class="btnquestions">
         </form>
     @endif
-    <article class="container margintop80">
+    <article class="container margintop40">
             <div class="row">
-                <div class="col-md-3" align="center">
-                    <img class="img-circle img-responsive" src="img/upload/avatar/{{$avatar}}">
-                    <p style="margin-top: 10px">
-                        <b>Rang:</b> 
-                        @if($top == "0")
-                            {{ $rank }}
-                        @endif
-                        @if($top == "1")
-                            Anfänger
-                        @endif
-                        @if($top == "2")
-                            Helfer
-                        @endif
-                        @if($top == "3")
-                            Sympathisant
-                        @endif
-                        @if($top >= "4")
-                            Held
-                        @endif
-                    </p>
-                    <p>
-                        <b>Top-Antwort:</b> {{ $top }} mal
-                    </p>
-                </div>                
-                <div class="col-sm-9 col-md-9">
-                    <div class="profil">                    
-                        <div class="row" style="border-bottom: 1px solid #dddddd; border-top: 1px solid #dddddd; padding: 10px 0">
-                            <div class="col-md-5">
-                                <label>Username:</label>
+                <div class="col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1">
+                    <div class="row">
+                    <div class="col-md-3" align="center">
+                        <img class="img-circle img-responsive" src="img/upload/avatar/{{$avatar}}">
+                        <p style="margin-top: 10px">
+                            <b>Rang:</b> 
+                            @if($top == "0")
+                                {{ $rank }}
+                            @endif
+                            @if($top == "1")
+                                Anfänger
+                            @endif
+                            @if($top == "2")
+                                Helfer
+                            @endif
+                            @if($top == "3")
+                                Sympathisant
+                            @endif
+                            @if($top >= "4")
+                                Held
+                            @endif
+                        </p>
+                        <p>
+                            <b>Top-Antwort:</b> {{ $top }} mal
+                        </p>
+                    </div>
+                    <div class="col-sm-9 col-md-9">
+                        <div class="profil">                    
+                            <div class="row" style="border-bottom: 1px solid #dddddd; border-top: 1px solid #dddddd; padding: 10px 0">
+                                <div class="col-md-3">
+                                    <label>Username:</label>
+                                </div>
+                                <div class="col-md-9">
+                                    <input class="form-control input-md" type="text" name="username" value="{{ $name }}" disabled="">
+                                </div>
                             </div>
-                            <div class="col-md-7">
-                                <label>{{ $name }}</label>
+                            <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
+                                <div class="col-md-3">
+                                    <label>Rang:</label>
+                                </div>
+                                <div class="col-md-9">
+                                    <input class="form-control input-md" type="text" name="username" value="{{ $rights }}" disabled="">
+                                </div>
+                            </div>
+                            <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
+                                <div class="col-md-3">
+                                    <label>Name:</label>
+                                </div>
+                                <div class="col-md-9">
+                                    <input class="form-control input-md" type="text" name="username" value="{{ $forename }} {{ $surname }}" disabled="">
+                                </div>
+                            </div>
+                            <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
+                                <div class="col-md-3">
+                                    <label>E-Mail:</label>
+                                </div>
+                                <div class="col-md-9">
+                                    <input class="form-control input-md" type="text" name="username" value="{{ $email }}" disabled="">
+                                </div>
+                            </div>
+                            <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
+                                <div class="col-md-3">
+                                    <label>Homepage:</label>
+                                </div>
+                                <div class="col-md-9">
+                                    <input class="form-control input-md" type="text" name="username" value="{{ $page }}" disabled="">
+                                </div>
+                            </div>
+                            <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
+                                <div class="col-md-3">
+                                    <label>Hochschule:</label>
+                                </div>
+                                <div class="col-md-9">
+                                    <input class="form-control input-md" type="text" name="username" value="{{ $college }}" disabled="">
+                                </div>
+                            </div>
+                            <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
+                                <div class="col-md-3">
+                                    <label>Studiengang:</label>
+                                </div>
+                                <div class="col-md-9">
+                                    <input class="form-control input-md" type="text" name="username" value="{{ $course }}" disabled="">
+                                </div>
                             </div>
                         </div>
-                        <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
-                            <div class="col-md-5">
-                                <label>Rang:</label>
-                            </div>
-                            <div class="col-md-7">
-                                <label>{{ $rights }}</label>
-                            </div>
-                        </div>
-                        <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
-                            <div class="col-md-5">
-                                <label>Name:</label>
-                            </div>
-                            <div class="col-md-7">
-                                <label>{{ $forename }} {{ $surname }}</label>
-                            </div>
-                        </div>
-                        <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
-                            <div class="col-md-5">
-                                <label>E-Mail:</label>
-                            </div>
-                            <div class="col-md-7">
-                                <label>{{ $email }}</label>
-                            </div>
-                        </div>
-                        <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
-                            <div class="col-md-5">
-                                <label>Hochschule:</label>
-                            </div>
-                            <div class="col-md-7">
-                                <label>{{ $college }}</label>
-                            </div>
-                        </div>
-                        <div class="row" style="border-bottom: 1px solid #dddddd; padding: 10px 0">
-                            <div class="col-md-5">
-                                <label>Studiengang:</label>
-                            </div>
-                            <div class="col-md-7">
-                                <label>{{ $course }}</label>
-                            </div>
-                        </div>
+                    </div>
                     </div>
                 </div>
             </div>
         </div>
-    </article>
-    <div class="row">
-        <div class="col-sm-12 col-md-12 column margintop10">
-            <button onclick="goBack()" class="btnquestions backbtn marginleft10"><i class="fa fa-btn fa-arrow-circle-left" aria-hidden="true"></i> Zurück</button>
+        <div class="row">
+            <div class="col-sm-12 col-md-12 column margintop10">
+                <button onclick="goBack()" class="btnquestions backbtn marginleft10"><i class="fa fa-btn fa-arrow-circle-left" aria-hidden="true"></i> Zurück</button>
+            </div>
         </div>
-    </div>
+    </article>
 </section>
 @endsection
