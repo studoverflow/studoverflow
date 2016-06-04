b5e4d5cdc928c8f0cdbd67cfc383e3f65d8d8113
Stephen Beck <mail@stephenbeck.de>
2016-06-05 00:55:59 +0200

    fdf

--- resources/views/imprint.blade.php
+++ resources/views/imprint.blade.php
@@ -2,66 +2,66 @@
 
 @section('content')
 <section id="imprint" class="container-fluid">
-	<h1 class="text-center margintop80">Impressum</h1>
-	<article class="container marginbottom40">
+	<h1 class="text-center margintop80 marginbottom40">Impressum</h1>
+	<article class="container">
 		<article class="row">
 			<div class="col-sm-12 col-md-12">
-				<div class="text-center">
+				<div class="col-sm-12 col-md-12 whiteboxdiv text-center marginbottom20">
 					Angaben gemäß § 5 TMG:
 					<h3>StudOverflow</h3>
+					<p>
 					Brauneggerstraße 55
-					<br />
 					78462 Konstanz
-					<br />
-					<br />
-					<br />
-					<b>Vertreten durch:</b>
-					<br />
-					Stephen Beck
-					<br />
+					</p>
+
+					Vertreten durch:
+					<p>
+					Stephen Beck>
 					Markus Jäckle
-					<br />
-					<br />
+					</p>
+
 					E-Mail:	mail@studoverflow.de
-					<br />
-					<br />
+
 					<h4>Hinweis auf EU-Streitschlichtung</h4>
+					<p>
 					Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:
-					<br />
 					<a href="http://ec.europa.eu/consumers/odr">http://ec.europa.eu/consumers/odr</a>
-					<br />
+				
 					Unsere E-Mail-Adresse finden sie oben im Impressum.
-					<br />
-					<br />
-					<br />
 				</div>
-				<h4>Haftungsausschluss (Disclaimer)</h4>
-				<h6>Haftung für Inhalte</h6>
-				Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen
-				verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte
-				fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
-				Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon
-				unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich.
-				Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
-				<br />
-				<br />
-				<h6>Haftung für Links</h6>
-				Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben.
-				Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten
-				ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt
-				der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht
-				erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer
-				Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
-				<br />
-				<br />
-				<h6>Urheberrecht</h6>
-				Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht.
-				Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen
-				der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten,
-				nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden,
-				werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet.
-				Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis.
-				Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+				<div class="col-sm-12 col-md-12 whiteboxdiv text-center marginbottom20">
+					<h4>Haftungsausschluss (Disclaimer)</h4>
+					<h6>Haftung für Inhalte</h6>
+					<p>
+					Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen
+					verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte
+					fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
+					Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon
+					unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich.
+					Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+					</p>
+					
+					<h6>Haftung für Links</h6>
+					<p>
+					Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben.
+					Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten
+					ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt
+					der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht
+					erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer
+					Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+					</p>
+
+					<h6>Urheberrecht</h6>
+					<p>
+					Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht.
+					Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen
+					der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten,
+					nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden,
+					werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet.
+					Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis.
+					Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+					</p>
+				</div>
 			</div>
 		</article>
 	</article>
