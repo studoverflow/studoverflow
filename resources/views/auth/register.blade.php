c0982aacaa8c880b6cd6739b9ceeae82a2427b5d
Stephen Beck <mail@stephenbeck.de>
2016-06-24 10:15:31 +0200

    ssss

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">
+                    <div class="panel-heading" id="registrationheader">
                         Registrierung
                     </div>
                     <div class="panel-body">
