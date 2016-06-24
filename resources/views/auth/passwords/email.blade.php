8184741fe0ce08b034a67ed0700ec4e35afd8107
Stephen Beck <mail@stephenbeck.de>
2016-06-24 10:48:20 +0200

    dd

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,11 +2,11 @@
 <!-- Main Content -->
 @section('content')
 <section id="email">
-    <div class="container">
+    <div class="container margintop40">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Passwort zurücksetzen</div>
+                    <div class="panel-heading" id="emailresetheader">Passwort zurücksetzen</div>
                     <div class="panel-body">
                         @if (session('status'))
                             <div class="alert alert-success">
@@ -28,8 +28,8 @@
                             </div>
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        <i class="fa fa-btn fa-envelope"></i> Passwort Reset-Link Senden
+                                    <button type="submit" class="btn btn-black">
+                                        <i class="fa fa-btn fa-envelope"></i> Senden
                                     </button>
                                 </div>
                             </div>
